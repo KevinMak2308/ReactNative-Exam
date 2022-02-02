7a602bf51eed1f674f8217b22cc7d09bfa9d76e1
Christian Kirschberg <ckirschberg@gmail.com>
2022-02-02 12:31:16 +0100

    todo

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Todo } from './entities/Todo';
 
 export default function App() {
   const [text, setText] = useState('')
+  const [todos, setTodos] = useState([] as Todo[])
+
+  const renderItem = ({ item }: { item: Todo }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+    </View>
+  );
+
+  const handleAddTodo = () => {
+    const todo = new Todo(Math.random().toString(), text)
+    setTodos((todos: Todo[]) => [...todos, todo])
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>Anton and Anne is hopefully happy today since he is learning React Native</Text>
       <TextInput value={text} onChangeText={setText} style={styles.textInput} />
+      <Button title="Add todo" onPress={handleAddTodo} />
+
+      <FlatList
+        data={todos}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -25,5 +46,14 @@ const styles = StyleSheet.create({
   textInput: {
     borderColor: '#000',
     borderWidth: 1
-  }
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
 });
---
+++ entities/Todo.ts
@@ -0,0 +1,4 @@
+export class Todo {
+    constructor(public id: string, public title: string) {
+    }
+}
