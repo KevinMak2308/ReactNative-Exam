836e7e94654d8f59b2633f0fdf2c8440ae006804
Christian Kirschberg <ckirschberg@gmail.com>
2022-02-02 11:37:24 +0100

    textinput

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('')
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Anton and Anne is hopefully happy today since he is learning React Native</Text>
+      <TextInput value={text} onChangeText={setText} style={styles.textInput} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderColor: '#000',
+    borderWidth: 1
+  }
 });
