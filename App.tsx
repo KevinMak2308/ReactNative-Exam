14704d62c220b43723a6a786df504594e58d2e64
ckirschberg <ckirschberg@gmail.com>
2022-02-23 09:13:44 +0100

    navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import HomeScreen from './screens/HomeScreen';
 import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
 import Screen3 from './screens/Screen3';
+import { StackParamList } from "./typings/navigations";
 
-const Stack = createNativeStackNavigator();
+
+const Stack = createNativeStackNavigator<StackParamList>();
+const Tab = createBottomTabNavigator();
+
+function ChatStackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
+      <Stack.Screen name="Screen3" component={Screen3} />
+    </Stack.Navigator>
+  );
+}
+
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-        <Stack.Screen name="Screen3" component={Screen3} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        {/* <Tab.Screen name="Discover" component={DiscoverScreen} /> */}
+        <Tab.Screen name="Chat" component={ChatStackNavigator} />
+        {/* <Tab.Screen name="Menu" component={MenuScreen} /> */}
+      </Tab.Navigator>
+
+
+
     </NavigationContainer>
   )
 }
---
+++ typings/navigations.ts
@@ -0,0 +1,5 @@
+export type StackParamList = {
+    Screen1: undefined;
+    Screen2: undefined;
+    Screen3: undefined;
+}
