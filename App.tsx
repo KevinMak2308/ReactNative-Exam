718d09db4923b9c388afa15cf62545304f315ac2
ckirschberg <ckirschberg@gmail.com>
2022-02-23 10:27:54 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import HomeScreen from './screens/HomeScreen';
 import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
 import Screen3 from './screens/Screen3';
 import chatReducer from './store/reducers/chat.reducer';
+import { StackParamList } from "./typings/navigations";
 
 
-const Stack = createNativeStackNavigator();
+
+const Stack = createNativeStackNavigator<StackParamList>();
+const Tab = createBottomTabNavigator();
+
+function ChatStackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
+      <Stack.Screen name="Screen3" component={Screen3} />
+    </Stack.Navigator>
+  );
+}
+
 
 
 const rootReducer = combineReducers({
@@ -24,12 +40,18 @@ export default function App() {
 
   return (
     <Provider store={store}>
+
+
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Screen1" component={Screen1} />
-          <Stack.Screen name="Screen2" component={Screen2} />
-          <Stack.Screen name="Screen3" component={Screen3} />
-        </Stack.Navigator>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          {/* <Tab.Screen name="Discover" component={DiscoverScreen} /> */}
+          <Tab.Screen name="Chat" component={ChatStackNavigator} />
+          {/* <Tab.Screen name="Menu" component={MenuScreen} /> */}
+        </Tab.Navigator>
+
+
+
       </NavigationContainer>
     </Provider>
   )
