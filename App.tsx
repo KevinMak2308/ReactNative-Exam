7f087acfa93dc3322be0c65c0578e45290c8703b
ckirschberg <ckirschberg@gmail.com>
2022-03-09 10:07:21 +0100

    redux-thunk

--- App.tsx
+++ App.tsx
@@ -3,14 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import HomeScreen from './screens/HomeScreen';
 import Screen1 from './screens/Screen1';
 import Screen2 from './screens/Screen2';
 import Screen3 from './screens/Screen3';
 import chatReducer from './store/reducers/chat.reducer';
 import { StackParamList } from "./typings/navigations";
-
 
 
 const Stack = createNativeStackNavigator<StackParamList>();
@@ -28,13 +28,17 @@ function ChatStackNavigator() {
 
 
 
+
+
+
 const rootReducer = combineReducers({
   chat: chatReducer,
   // user: UserReducer,
   // posts: PostReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+// const store = createStore(rootReducer);
 
 export default function App() {
 
